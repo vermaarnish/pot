19a7b1b9080c3d1e9bff936ec62ab09018e7fb17
Arnish verma <78898740+vermaarnish@users.noreply.github.com>
2023-06-26 23:40:39 +0530

    Create question5.cpp

---
+++ assignment-2/question5.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdC++.h>
+using namespace std;
+   int maximumProduct(vector<int>& nums) {
+       int n=nums.size();
+       sort(nums.begin(),nums.end());
+       return max((nums[n-1]*nums[n-2]*nums[n-3]),(nums[0]*nums[1]*nums[n-1]));  
+    }
+int main(){
+   vector<int>nums={1,2,3};
+   cout<<maximumProduct(nums)<<endl;
+   return 0;
+}
